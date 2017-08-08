bfae4cf3c0c3d796d88627d0f0e7a81daabd2b64
Lukáš Jech <klukule@moowdesign.eu>
2017-08-08 14:31:29 +0200

    [GUI] Readded GUI code

---
+++ Albion/Merlin.GUI/Controls/Spacer.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Merlin.GUI.Controls
+{
+    /// <summary>
+    /// Interaction logic for Spacer.xaml
+    /// </summary>
+    public partial class Spacer : UserControl
+    {
+        public Spacer()
+        {
+            InitializeComponent();
+        }
+    }
+}

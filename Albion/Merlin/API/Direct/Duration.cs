6e386babd7419b20785f3ff656af6fc0c9f71829
Lukáš Jech <klukule@moowdesign.eu>
2017-08-21 21:37:39 +0200

    [API] Some types are now structures instead of classes + struct generating is working

--- Albion/Merlin/API/Direct/Duration.cs
+++ Albion/Merlin/API/Direct/Duration.cs
@@ -22,7 +22,7 @@ using Albion.Common.Time;
 namespace Merlin.API.Direct
 {
     /* Internal type: ajv */
-    public partial class Duration
+    public partial struct Duration
     {
         private static List<MethodInfo> _methodReflectionPool = new List<MethodInfo>();
         private static List<PropertyInfo> _propertyReflectionPool = new List<PropertyInfo>();
